706a9d4d9d6e353bb63b1a2d9f24817142f28d68
Tim Honchel <tim.honchel@gmail.com>
2023-08-28 16:12:47 -0400

    add models
    add PointAverages model
    
    add PointAverages model

---
+++ Fantasy.Logic/Models/PointAverages.cs
@@ -0,0 +1,22 @@
+﻿namespace Fantasy.Logic.Models
+{
+    public class PointAverages
+    {
+        public double QB { get; set; }
+        public double RB1 { get; set; }
+        public double RB2 { get; set; }
+        public double WR1 { get; set; }
+        public double WR2 { get; set; }
+        public double TE { get; set; }
+        public double FLEX { get; set; }
+        public double DEF { get; set; }
+        public double K { get; set; }
+        public double total { get; set; }
+        public double FAQB { get; set; }
+        public double FARB { get; set; }
+        public double FAWR { get; set; }
+        public double FATE { get; set; }
+        public double FADEF { get; set; }
+        public double FAK { get; set; }
+    }
+}
