dcc9cdee6a4c64a8e3c139b596271b78cb018661
Tim Honchel <tim.honchel@gmail.com>
2023-08-28 12:48:27 -0400

    set up basic architecture

---
+++ Fantasy.API/Controllers/MainController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fantasy.API.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class MainController : ControllerBase
+    {
+        
+    }  
+}
