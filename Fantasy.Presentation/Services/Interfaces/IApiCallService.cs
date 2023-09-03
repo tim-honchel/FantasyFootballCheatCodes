08f05fb6395511e1b6842051e8e85809d9b2e14e
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2023-09-02 21:07:58 -0400

    add API call service interface

---
+++ Fantasy.Presentation/Services/Interfaces/IApiCallService.cs
@@ -0,0 +1,31 @@
+﻿using Fantasy.Presentation.Data.RequestObjects;
+using Fantasy.Presentation.Data.RequestObjects.Fantasy.Logic.Requests;
+using Fantasy.Presentation.Data.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fantasy.Presentation.Services.Interfaces
+{
+    public interface IApiCallService
+    {
+        Task<CostAnalysisViewModel> CostAnalysis(CostAnalysisRequestObject request);
+        Task<FileContentResult> CsvStarters(CsvStartersRequestObject request);
+        Task<FileContentResult> CsvSuggestedRosters(SuggestedRostersRequestObject request);
+        Task<List<PlayerViewModel>> ExpectedValue(ExpectedValueRequestObject request);
+        Task<List<PlayerESPNViewModel>> EspnPlayers(EspnPlayersRequestObject request);
+        Task<RulesESPNViewModel> EspnRules(EspnRulesRequestObject request);
+        Task<RulesViewModel> LeagueRules(LeagueRulesRequestObject request);
+        Task<List<PlayerViewModel>> PlayerProjections(PlayerProjectionsRequestObject request);
+        Task<PointAveragesViewModel> PointAverages(PointAveragesRequestObject request);
+        Task<List<RosterViewModel>> PossibleRosters(PossibleRostersRequestObject request);
+        Task<List<PlayerViewModel>> RelativePoints(RelativePointsRequestObject request);
+        Task<RosterViewModel> StrongerRoster(StrongerRosterRequestObject request);
+        Task<RosterViewModel> StrongRoster(StrongRosterRequestObject request);
+        Task<List<DraftBoardViewModel>> SuggestedRosters(SuggestedRostersRequestObject request);
+        Task<List<PlayerViewModel>> Tags(TagsRequestObject request);
+        Task<CountByIDViewModel> TopRosterFrequency(TopRosterFrequencyRequestObject request);
+        Task<List<PlayerViewModel>> TopRosterPercent(TopRosterPercentRequestObject request);
+        Task<List<int>> TopRosterPlayers(TopRosterPlayersRequestObject request);
+        
+
+    }
+}
