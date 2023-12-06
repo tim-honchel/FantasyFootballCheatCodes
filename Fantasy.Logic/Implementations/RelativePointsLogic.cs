83de571f02cc1dc9616f1cc8b0ab2d84b1352fdc
Tim Honchel <94769316+tim-honchel@users.noreply.github.com>
2023-12-05 20:58:26 -0500

    add combo positions to relativePoints endpoint

--- Fantasy.Logic/Implementations/RelativePointsLogic.cs
+++ Fantasy.Logic/Implementations/RelativePointsLogic.cs
@@ -12,15 +12,38 @@ namespace Fantasy.Logic.Implementations
         {
             RelativePointsResponse response = new();
 
+            Dictionary<string, List<string>> relevantComboPositions = GetRelevantComboPositions(request.PointAverages);
+
             foreach (Player player in request.Players)
             {
-                UpdatePlayerRelativePoints(player, request.PointAverages);
+                UpdatePlayerRelativePointsBasePosition(player, request.PointAverages);
+                UpdatePlayerRelativePointsComboPosition(player, relevantComboPositions, request.PointAverages);
             }
 
             response.Players = request.Players;
+            response.Success = true;
             return response;
         }
-        public void UpdatePlayerRelativePoints(Player player, PointAverages averages)
+
+        public Dictionary<string, List<string>> GetRelevantComboPositions(PointAverages averages)
+        {
+            Dictionary<string, List<string>> relevantComboPositions = new Dictionary<string, List<string>>();
+
+            Dictionary<string, List<string>> comboPositionsAndTheirBasePositions = PositionDictionaryService.GetComboPositionsAndTheirBasePositions();
+
+            foreach (KeyValuePair<string, double> average in averages.AverageByPosition)
+            {
+                string position = average.Key.Substring(0, average.Key.Length - 1);
+
+                if (average.Value > 0 && comboPositionsAndTheirBasePositions.Keys.Contains(position) && !relevantComboPositions.Keys.Contains(position))
+                {
+                    relevantComboPositions.Add(position, comboPositionsAndTheirBasePositions[position]);
+                }
+            }
+
+            return relevantComboPositions;
+        }
+        public void UpdatePlayerRelativePointsBasePosition(Player player, PointAverages averages)
         {
             string position = player.Position;
             double average1 = averages.AverageByPosition.ContainsKey($"{position}1") ? averages.AverageByPosition[$"{position}1"] : 0;
@@ -43,6 +66,32 @@ namespace Fantasy.Logic.Implementations
             if (freeAgent > 0)
             {
                 player.FA = Math.Round(player.WeeklyPoints - freeAgent, 2);
+            }
+        }
+
+        public void UpdatePlayerRelativePointsComboPosition(Player player, Dictionary<string, List<string>> relevantComboPositions, PointAverages averages)
+        {
+            foreach (KeyValuePair<string, List<string>> relevantComboPosition in relevantComboPositions)
+            {
+                if (relevantComboPosition.Value.Contains(player.Position))
+                {
+                    double average1 = averages.AverageByPosition.ContainsKey($"{relevantComboPosition.Key}1") ? averages.AverageByPosition[$"{relevantComboPosition.Key}1"] : 0;
+                    double average2 = averages.AverageByPosition.ContainsKey($"{relevantComboPosition.Key}2") ? averages.AverageByPosition[$"{relevantComboPosition.Key}2"] : 0;
+                    double average3 = averages.AverageByPosition.ContainsKey($"{relevantComboPosition.Key}3") ? averages.AverageByPosition[$"{relevantComboPosition.Key}3"] : 0;
+
+                    if (average1 > 0)
+                    {
+                        player.RelativePoints[$"{relevantComboPosition.Key}1"] = Math.Round(player.WeeklyPoints - average1, 2);
+                    }
+                    if (average2 > 0)
+                    {
+                        player.RelativePoints[$"{relevantComboPosition.Key}2"] = Math.Round(player.WeeklyPoints - average2, 2);
+                    }
+                    if (average3 > 0)
+                    {
+                        player.RelativePoints[$"{relevantComboPosition.Key}3"] = Math.Round(player.WeeklyPoints - average3, 2);
+                    }
+                }
             }
         }
     }
